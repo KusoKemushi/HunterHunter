b9b4634b89ae2391df0d786b498343c3209eb18b
Kemushi <tigeramano@gmail.com>
2022-02-24 10:48:15 -0600

    Initial Commit

---
+++ HunterHunter/HunterHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HunterHunterApp.swift
+//  HunterHunter
+//
+//  Created by Student on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct HunterHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
